21c92a40eba2aaf0e97de1668a3f5af923f2d4f5
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-08 16:26:33 +0900

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
         
     }
 
@@ -42,6 +43,13 @@ public class Main {
         System.out.println("Задача 5");
         for (int i = 1904; i <= 2096; i+=4) {
             System.out.println(i + " год является високосным");
+        }
+    }
+    
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.println(i);
         }
     }
     
