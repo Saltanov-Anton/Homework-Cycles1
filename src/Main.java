79efaf1997c6778cd925f853de5240bb51be750c
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-08 16:53:45 +0900

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
         
     }
 
@@ -58,6 +59,16 @@ public class Main {
         System.out.println("Задача 7");
         for (int i = 1; i <= 512; i*=2) {
             System.out.println(i);
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int contribution = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total += contribution;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
     }
     
